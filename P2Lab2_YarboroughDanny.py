1009c375f6203c2bbf87cb7e8f37f161739d9dc5
DannyYar01 <99427823+DannyYar01@users.noreply.github.com>
2022-05-06 02:50:30 -0400

    Add files via upload

---
+++ P2Lab2_YarboroughDanny.py
@@ -0,0 +1,12 @@
+current_price = int(input())
+last_months_price = int(input())
+change = current_price - last_months_price
+mortgage = current_price * 0.051 / 12
+print('This house is $', end='')
+print(current_price, end='. ')
+print('The change is $', end='')
+print(change, end=' ')
+print('since last month.')
+print('The estimated monthly mortgage is $', end='')
+print(f'{mortgage:.2f}.')
+   
