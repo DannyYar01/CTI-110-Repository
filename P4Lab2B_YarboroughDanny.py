db34f3249b8cdb864e0b8725a8bf5151cc87c826
DannyYar01 <99427823+DannyYar01@users.noreply.github.com>
2022-05-06 03:29:01 -0400

    Add files via upload

---
+++ P4Lab2B_YarboroughDanny.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+t.pensize(30)
+t.color("purple")
+t.circle(100,180)
+t.left(90)
+t.forward(193)
+
+t.up()
+t.forward(75)
+t.down()
+
+t.pensize(30)
+t.color("purple")
+t.right(-45)
+t.forward(100)
+t.right(85)
+t.forward(100)
+t.backward(200)
